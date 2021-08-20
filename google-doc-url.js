fe6b576d4b964c0e0afd1eec0dd915fd75881d2b
myLawZone <88707008+myLawZone@users.noreply.github.com>
2021-08-20 08:08:37 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AreZQ42_3M-4Ift9P79aK_XR7L0XmVCrWlKqKX3UoBg/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y7z-mhezjzU2ANuZjjStW104igfCbj44iJh9JWcpufw/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
